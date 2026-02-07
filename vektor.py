840d2f3d6b6b177d4a90e84f5680c01bdb97ed10
putri-apps <putri.apps89@gmail.com>
2026-02-07 10:02:27 +0700

    Add files via upload

---
+++ vektor.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  7 09:45:08 2026
+
+@author: putri-pramesti
+"""
+
+# vektor python numpy with range value
+import numpy as np
+print("vektor default python\n")
+a = np.arange(1,20,1)
+b = np.arange(1,20,2)
+import numpy as np
+print (" \n vektor via numpy \n")
+# vektor via numpy
+c = np.array ([1,2,3,4,5])
+d = np.array ([1.5, 2.5, 5, 6, 7])
+print(a)
+print(b)
+print(a.ndim)
+print(a.shape)
